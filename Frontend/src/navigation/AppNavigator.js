ac4f37aa4d5d014434de2ffb398163e24b9106cd
Shirish <np03cs4s210195@heraldcollege.edu.np>
2024-05-25 00:35:33 +0545

    Stacked Navigation

--- Frontend/src/navigation/AppNavigator.js
+++ Frontend/src/navigation/AppNavigator.js
@@ -1,46 +1,51 @@
-
+import { create } from "apisauce";
+import { Settings } from "react-native";
 import React, { useContext } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import SplashScreen from "../screens/splash";
-import RegisterScreen from "../screens/auth/Register";
+import Setting from "../screens/Settings";
+import Results from "../screens/results";
 import Dashboard from "../screens/Dashboard";
 import LoginScreen from "../screens/auth/Login";
-import { create } from "apisauce";
-import ForgotPasswordScreen from "../screens/auth/ForgotPassword";
+import FAQScreen from "../screens/FAQ/FAQScreen";
+import RegisterScreen from "../screens/auth/Register";
 import CarbonEmission from "../screens/CarbonEmissions";
+import { NavigationContainer } from "@react-navigation/native";
+import ArticleDetailScreen from "../screens/ArticleDetailScreen";
+import ForgotPasswordScreen from "../screens/auth/ForgotPassword";
+import ChangePasswordScreen from "../screens/auth/ChangePassword";
 import { fontSize } from "react-native-rapi-ui/constants/typography";
-import Results from "../screens/green/results";
-import { Settings } from "react-native";
-import Setting from "../screens/Settings";
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
-export default function AppNavigator(){
-  return(
+export default function AppNavigator() {
+  return (
     <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen screenOptions={{
+      <Stack.Navigator
+        screenOptions={{
           headerStyle: {
             backgroundColor: '#007bff', // Change header background color
           },
           headerTintColor: '#fff', // Change text color of header title
           headerTitleStyle: {
-            fontWeight: 'bold',fontSize: 9 // Optionally adjust header title font weight
+            fontWeight: 'bold', // Optionally adjust header title font weight
+            fontSize: 18, // Adjust header title font size
           },
-        }} name="Login" component={LoginScreen} />
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name= "CarbonEmissions" component={CarbonEmission} />
-        <Stack.Screen name= "Results" component={Results} />
-        <Stack.Screen name ="Setting" component={Setting} />
+        <Stack.Screen name="CarbonEmissions" component={CarbonEmission} />
+        <Stack.Screen name="Results" component={Results} />
+        <Stack.Screen name="Setting" component={Setting} />
+        <Stack.Screen name="FAQScreen" component={FAQScreen} />
+        <Stack.Screen name="ArticleDetail" component={ArticleDetailScreen} />
+        <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
 
 
 
